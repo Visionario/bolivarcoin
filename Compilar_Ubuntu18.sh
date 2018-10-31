a2de9cd6f027045a1587454d5c4b551dbf97bf0d
Visionario <visionario@gmail.com>
2018-10-31 17:48:19 -0400

    Better dependency order to install and avoid conflicts

--- Compilar_Ubuntu18.sh
+++ Compilar_Ubuntu18.sh
@@ -1,7 +1,31 @@
-# scrypt para instalar la Wallet de BolivarCoin en GNU/Linux
+# script para instalar la Wallet de BolivarCoin en GNU/Linux UBUNTU 18.x
 
 # Actualizar el sistema e instalar dependencias
-sudo apt-get install libssl1.0-dev qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools build-essential libdb++-dev libboost-all-dev libqrencode-dev libminiupnpc-dev qrencode libqrencode-dev git automake autoconf pkg-config libcurl-openssl1.0-dev libjansson-dev libgmp-dev make g++ gcc-6 g++-6
+# Éste es el orden de instalación de dependencias más apropiado 
+# para la mayoría de las instalaciones Ubuntu 18
+sudo apt-get install libssl1.0-dev 
+
+sudo apt-get install qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools 
+
+sudo apt-get install build-essential 
+
+sudo apt-get install libdb++-dev 
+
+sudo apt-get install libboost-all-dev 
+
+sudo apt-get install libqrencode-dev libminiupnpc-dev 
+
+sudo apt-get install libjansson-dev 
+
+sudo apt-get install libgmp-dev 
+
+# Posible error en esta línea 
+sudo apt-get install libcurl-openssl1.0-dev 
+
+sudo apt-get install git automake autoconf pkg-config make g++ gcc-6 g++-6 qrencode 
+
+
+# Add Bitcoin repository PPA
 sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install libdb4.8-dev libdb4.8++-dev
